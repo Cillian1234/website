f8b4e0f624a80ec03f4fdced724f10f4903a43ac
Cillian <126096645+CillianSmee@users.noreply.github.com>
2023-04-11 03:23:18 +0100

    Minor css changes + start of form validation
    Changed some colours and started working on validating shipping details form

--- JavaScript/JQuery.js
+++ JavaScript/JQuery.js
@@ -3,49 +3,37 @@ $(document).ready(function(){
         $("#menu").slideToggle(300);
     });
 
-    $("#form").validate({
-        rules: {
-            name: {
-                required: true,
-                // minLength: 5
-            },
-            address: "required",
-            phoneNumber: {
-                required: true,
-                // pattern: "[0-9]{3}-[0-9]{2}-[0-9]{3}"
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            gender: {
-                required: true
-            },
-            terms: {
-                required: true
-            }
-        },
+    $("#emailError").hide();
+    $("#addressError").hide();
+    $("#cityError").hide();
+    $("#countryError").hide();
+    $("#shippingError").hide();
+    $("#termsError").hide();
 
-        messages : {
-            name: {
-                required: "Required",
-                minLength: "Minimum length of 5"
-            },
-            address: "Please enter your address",
-            phoneNumber: {
-                required: "Please enter your phone number",
-                pattern: "Please match the required pattern"
-            },
-            email: {
-                required: "You must enter an email address",
-                email: "The email should be in the format: abc@domain.tld"
-            },
-            gender: {
-                required: "Required"
-            },
-            terms: {
-                required: "Required"
-            }
+    let emailError = false,
+        addressError = false,
+        cityError = false,
+        countryError = false,
+        shippingError = false,
+        termsError = false;
+
+    $("#Email").focusout(function (){validateEmail();});
+    $("#Address").focusout(function (){validateAddress();});
+    $("#City").focusout(function (){validateCity();});
+    $("#Country").focusout(function (){validateCountry();});
+    $("#Shipping").focusout(function (){validateShipping();});
+    $("#Terms").focusout(function (){validateTerms();});
+
+    function validateEmail() {
+        let pattern = /^[a-zA-Z]@[a-zA-Z].[a-zA-Z]*$/,
+            email = $("#Email").val();
+        if (pattern.test(email) && email !== "") {
+            $("#emailError").hide();
+        } else {
+            $("#emailError").html("Email should be in format ('Email@example.com')");
+            $("#emailError").show();
+            emailError = true;
         }
-    });
+    }
+
 });
